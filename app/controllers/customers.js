ee256b2b058a8d31010d143158377295350073cb
Jary <jarysitraka@outlook.com>
2016-09-05 10:14:09 +0300

    detail customer and Intervention

--- app/controllers/customers.js
+++ app/controllers/customers.js
@@ -17,16 +17,29 @@ myApp.controller('CustomersCtrl',function ($scope, $rootScope, CustomersFactory,
 
 
 myApp.controller('CustomersDetailsCtrl',function ($scope, $rootScope, CustomersFactory,$stateParams,$cookies) {
+
+    $scope.loading=true;
+
     $scope.customer = CustomersFactory.getCustomer($stateParams.id).then(function (customer) {
-        $scope.$parent.pageTitle="Customer.detail";//à mettre sur tous les controllers
+        $scope.$parent.pageTitle=customer.FullName;//à mettre sur tous les controllers
         $scope.$parent.cookieUsername=$cookies.getObject('connected');//à mettre sur tous les controllers
         $scope.customer=customer;
         $scope.interventionsCustomer = customer.Interventions;
-        
-        //console.log("reo e"+customer.Interventions[0].Remarks);
+        $scope.devicesCustomer = customer.Devices;
+
         Materialize.toast("mety", 3000, 'green');
         //Materialize.toast("liste bien recuper", 3000, 'blue')
     }, function (msg) {
         Materialize.toast(msg, 3000, 'red');
     });
+
+    $scope.projectsCustomer = CustomersFactory.getProjectsCustomer($stateParams.id).then(function (projects) {
+        $scope.projectsCustomer = projects;
+
+        $scope.loading=false;
+    }, function (msg) {
+        Materialize.toast(msg, 3000, 'red');
+    });
+
+
 });
--- app/services/customers.js
+++ app/services/customers.js
@@ -4,6 +4,7 @@
 myApp.factory('CustomersFactory',function ($http, $q) {
     var factory ={
         customers : false,
+        projects :false,
         getCustomers: function () {
             var deferred = $q.defer();
             if(factory.customers!=false){
@@ -23,6 +24,7 @@ myApp.factory('CustomersFactory',function ($http, $q) {
             }
             return deferred.promise;
         },
+
         getCustomer: function (id){
             var deferred = $q.defer();
             var customer = {};
@@ -38,6 +40,26 @@ myApp.factory('CustomersFactory',function ($http, $q) {
                 deferred.reject(msg);
             });
 
+            return deferred.promise;
+        },
+
+        getProjectsCustomer: function (id) {
+            var deferred = $q.defer();
+            if(factory.projects!=false){
+                deferred.resolve(factory.projects);
+            }
+            else {
+                $http.get('http://localhost:3821/api/Customers/'+id+'/projects')
+                    .success(function (data) {
+                        factory.projects = angular.fromJson(data);
+                        //console.log(factory.customers);
+                        deferred.resolve(factory.projects);
+                    })
+                    .error(function (response) {
+                        Materialize.toast(response, 3000, 'red');
+                        deferred.reject(response);
+                    });
+            }
             return deferred.promise;
         }
     };
