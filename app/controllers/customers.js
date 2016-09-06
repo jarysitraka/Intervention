a72094514877af35cbaf851a14cdeb545da78d8f
Jary <jarysitraka@outlook.com>
2016-09-06 21:05:57 +0300

    Intervention Detail new look

--- app/controllers/customers.js
+++ app/controllers/customers.js
@@ -2,13 +2,14 @@
  * Created by jarys on 21/07/2016.
  */
 myApp.controller('CustomersCtrl',function ($scope, $rootScope, CustomersFactory,$cookies) {
+    $scope.loading=true;
     $scope.customers = [];
     CustomersFactory.getCustomers().then(function (CustomersFactory) {
         $scope.$parent.pageTitle='Customers';//à mettre sur tous les controllers
         $scope.$parent.cookieUsername=$cookies.getObject('connected');//à mettre sur tous les controllers
 
         $scope.customers = CustomersFactory;
-
+        $scope.loading=false;
         //Materialize.toast("liste bien recuper", 3000, 'blue')
     }, function (msg) {
         Materialize.toast(msg, 3000, 'red');
--- app/controllers/intervention.js
+++ app/controllers/intervention.js
@@ -14,12 +14,13 @@ myApp.controller('InterventionCtrl',function ($scope, $rootScope, InterventionFa
 
 
 myApp.controller('InterventionDetailsCtrl',function ($scope, $rootScope, InterventionFactory,$stateParams,$cookies) {
+    $scope.loading=true;
     $scope.intervention = InterventionFactory.getIntervention($stateParams.id).then(function (intervention) {
         $scope.$parent.pageTitle="Intervention.detail";//à mettre sur tous les controllers
         $scope.$parent.cookieUsername=$cookies.getObject('connected');//à mettre sur tous les controllers
         $scope.intervention=intervention[0];
         $scope.interventionGroup =$scope.intervention.Group;
-        
+        $scope.loading=false;
         //console.log("reo e"+customer.Interventions[0].Remarks);
         Materialize.toast("mety", 3000, 'green');
         //Materialize.toast("liste bien recuper", 3000, 'blue')
