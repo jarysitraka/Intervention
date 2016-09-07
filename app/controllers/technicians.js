f7ddb3a98b4ccb149fef8cee226dce2155a3830a
Jary <jarysitraka@outlook.com>
2016-09-08 00:18:57 +0300

    Begin technicians management

--- app/controllers/technicians.js
+++ app/controllers/technicians.js
@@ -1,3 +1,17 @@
 myApp.controller('TechniciansCtrl',function ($scope, $rootScope, TechnicianFactory,$cookies) {
 
+    $scope.loading=true;
+    $scope.technicians = [];
+    TechnicianFactory.getTechnicians().then(function (TechnicianFactory) {
+        $scope.$parent.pageTitle='Technicians';//à mettre sur tous les controllers
+        $scope.$parent.cookieUsername=$cookies.getObject('connected');//à mettre sur tous les controllers
+
+        $scope.technicians = TechnicianFactory;
+        $scope.loading=false;
+        //Materialize.toast("liste bien recuper", 3000, 'blue')
+    }, function (msg) {
+        Materialize.toast(msg, 3000, 'red');
+    });
+    
+    
 });
--- app/services/technicians.js
+++ app/services/technicians.js
@@ -1,4 +1,45 @@
 
 myApp.factory('TechnicianFactory',function ($http, $q) {
-   
+
+    var factory ={
+        technicians : false,
+        technician : {},
+        getTechnicians: function () {
+            var deferred = $q.defer();
+            if(factory.technicians!=false){
+                deferred.resolve(factory.technicians);
+            }
+            else {
+                $http.get('http://localhost:3821/api/Technicians')
+                    .success(function (data) {
+                        factory.technicians = angular.fromJson(data);
+                        //console.log(factory.customers);
+                        deferred.resolve(factory.technicians);
+                    })
+                    .error(function (response) {
+                        Materialize.toast(response, 3000, 'red');
+                        deferred.reject(response);
+                    });
+            }
+            return deferred.promise;
+        },
+        getTechnician: function (id){
+            var deferred = $q.defer();
+
+            $http.get('http://localhost:3821/api/Technician/'+id+'')
+                .success(function (data) {
+                    factory.technician = angular.fromJson(data);
+                    //console.log(factory.customers);
+                    deferred.resolve(factory.technician);
+                })
+                .error(function (response) {
+                    Materialize.toast(response, 3000, 'red');
+                    deferred.reject(response);
+                });
+            return deferred.promise;
+        }
+    };
+    return factory;
+
+
 });
